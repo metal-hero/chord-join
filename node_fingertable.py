53b63663ee8428e2b940d89a7d12793a06df26b4
root <root@debianSDU.sdu.edu.kz>
2015-10-15 02:15:43 +0600

    Finger table v0.3

--- node_fingertable.py
+++ node_fingertable.py
@@ -14,15 +14,34 @@ def between(p_k, p_m, p_n):
         return False
     if p_k == 'None' or p_m == 'None' or p_n == 'None':
         return False
-    #k = ring_hash(str(p_k))
-    #m = ring_hash(str(p_m))
-    #n = ring_hash(str(p_n))
-    k = int(str(p_k))
-    m = int(str(p_m))
-    n = int(str(p_n))
+    k = ring_hash(str(p_k))
+    m = ring_hash(str(p_m))
+    n = ring_hash(str(p_n))
+    #k = int(str(p_k))
+    #m = int(str(p_m))
+    #n = int(str(p_n))
     if m == n:
         return True
     return (m==n or (k<m or k>m)) and (k-m)%2**r <= (n-m)%2**r
+
+def strictly_between(p_k, p_m, p_n):
+    r = 6
+    if p_k is None or p_m is None or p_n is None:
+        return False
+    if p_k == 'None' or p_m == 'None' or p_n == 'None':
+        return False
+    k = ring_hash(str(p_k))
+    m = ring_hash(str(p_m))
+    n = ring_hash(str(p_n))
+    #k = int(str(p_k))
+    #m = int(str(p_m))
+    #n = int(str(p_n))
+    if m ==n and k!=m:
+        return True
+    elif m!=n and (k-m)%2**r < (n-m)%2**r:
+        return True
+    else:
+        False
 
 def ring_hash(s):
     m = 6
@@ -41,7 +60,7 @@ class ChordNode(object):
 
         self.pred = None
         self.node_id = str(node_id)
-        self.succ = None
+        # self.succ == fingers[0]
 
         self.M = 2**self.m
         self.fingers = [None for i in range(self.m)]
@@ -60,18 +79,17 @@ class ChordNode(object):
         channel.basic_consume(self.findsucc_callback,
                               queue='findsucc' + str(node_id),
                               no_ack=True)
-        channel.basic_consume(self.herepred_callback,
-                              queue='herepred' + str(node_id),
-                              no_ack=True)
-        channel.basic_consume(self.findpred_callback,
-                              queue='findpred' + str(node_id),
-                              no_ack=True)
-        channel.basic_consume(self.notify_callback,
-                              queue='notify' + str(node_id),
-                              no_ack=True)
-
+        #channel.basic_consume(self.findpred_callback,
+        #                      queue='findpred' + str(node_id),
+        #                      no_ack=True)
+        #channel.basic_consume(self.herepred_callback,
+        #                      queue='herepred' + str(node_id),
+        #                      no_ack=True)
+        #channel.basic_consume(self.notify_callback,
+        #                      queue='notify' + str(node_id),
+        #                      no_ack=True)
         if str(node_id) == str(known_id):
-            self.succ = str(node_id)
+            self.fingers[0] = str(node_id)
             self.pred = str(node_id)
             print "Listening"
         else:
@@ -84,38 +102,39 @@ class ChordNode(object):
 
     def closest_preceding_finger(self, id):
         for i in range(self.m-1,-1,-1):
-            if between(self.finger[i],self.node_id,id):
+            if strictly_between(self.finger[i],self.node_id,id):
                 return self.finger[i]
         return self.node_id
 
 
     def findsucc_callback(self, channel, _, _2, body):
-        print "body /" + str(body)
-        print "this node /" + str(self.node_id)
-        print "my succ node /" + str(self.succ)
+        # print "body /" + str(body)
+        # print "this node /" + str(self.node_id)
+        # print "my succ node /" + str(self.fingers[0])
         data = json.loads(body)
-        pred = data['curr']
-        key = (int(pred)+2**(int(data['i'])))%2**self.m
-        if between(key, self.node_id, self.succ):
+        pred = str(data['curr'])
+        # key = (int(pred)+2**(int(data['i'])))%2**self.m
+        key = (ring_hash(str(pred))+2**(ring_hash(str(data['i']))))%2**self.m
+        if between(key, self.node_id, self.fingers[0]):
             print "smth"
-            data = {'next': self.succ, 'curr': self.node_id, 'i':data['i']}
+            data = {'next': self.fingers[0], 'curr': self.node_id, 'i':data['i']}
             channel.basic_publish(exchange='',
                                   routing_key='heresucc'+str(pred),
                                   body=json.dumps(data))
             print "between"
         else:
             channel.basic_publish(exchange='',
-                                  routing_key='findsucc'+str(self.closest_preceding_finger(self.succ)),
+                                  routing_key='findsucc'+str(self.closest_preceding_finger(key)),
                                   body=str(body))
             print "not between"
 
 
     def heresucc_callback(self, channel, _, _2, body):
-        print 'heresucc body/' + str(body)
+        # print 'heresucc body/' + str(body)
         data = json.loads(body)
-        self.succ = data['next']
+        #self.fingers[0] = data['next']
         pred = data['curr']
-        index = int(data['i'])
+        index = int(data['i']) % int(self.m)
         self.fingers[index] = data['next']
         if index==0:
             threading.Thread(target=self.start_stabilise).start()
@@ -126,21 +145,18 @@ class ChordNode(object):
                                   routing_key='heresucc'+str(pred),
                                   body=json.dumps(data))
 
-        print "heresucc body/" + str(body)
-        print "heresucc this node/" + str(self.node_id)
-        print "heresucc my succ node/" + str(self.succ)
+        # print "heresucc body/" + str(body)
+        # print "heresucc this node/" + str(self.node_id)
+        # print "heresucc my succ node/" + str(self.fingers[0])
 
 
     def findpred_callback(self, channel, _, _2, body):
         #print "find body /" + str(body)
         #print "find this node /" + str(self.node_id)
         #print "find my pred node /" + str(self.pred)
-        data = {'pred': 'None', 'x': str(self.pred)}
-        if between(self.pred, body, self.node_id):
-            data['pred'] = 'None'
         channel.basic_publish(exchange='',
                                   routing_key='herepred'+str(body),
-                                  body=json.dumps(data))
+                                  body=str(self.pred))
 
 
     def notify_callback(self, channel, _, _2, body):
@@ -153,18 +169,15 @@ class ChordNode(object):
 
     def herepred_callback(self, channel, _, _2, body):
         #print "--I am '"+ str(self.node_id) +"' ."
-        #print "--Updated successor to '"+ str(self.succ) +"' ."
+        #print "--Updated successor to '"+ str(self.fingers[0]) +"' ."
         #print "--Updated predecessor to '"+ str(self.pred) +"' ."
-        data = json.loads(body)
-        #print data
-        if data['pred'] != 'None':
-            self.succ = data['pred']
-        x = data['x']
+        if between(body,self.node_id, self.fingers[0]):
+            self.fingers[0] = body
         channel.basic_publish(exchange='',
-                                routing_key='notify'+str(self.succ),
+                                routing_key='notify'+str(self.fingers[0]),
                                 body=str(self.node_id))
         print "I am '"+ str(self.node_id) +"' ."
-        print "Updated successor to '"+ str(self.succ) +"' ."
+        print "Updated successor to '"+ str(self.fingers[0]) +"' ."
         print "Updated predecessor to '"+ str(self.pred) +"' ."
 
     '''
@@ -195,10 +208,10 @@ class ChordNode(object):
             self.pred = n
 
     def stabilise(self):
-        x = self.succ.pred
-        if between(x,self.node_id,self.succ):
-            self.succ = x
-        self.succ.notify(self)
+        x = self.fingers[0].pred
+        if between(x,self.node_id,self.fingers[0]):
+            self.fingers[0] = x
+        self.fingers[0].notify(self)
     '''
 
     def fix_fingers(self, channel):
@@ -208,19 +221,20 @@ class ChordNode(object):
         #    channel.basic_publish(exchange='',
         #                      routing_key='findsucc' + str(key),
         #                      body=json.dumps(data))
-        print self.fingers
+        print "I'm  "+str(self.node_id)
         print '-------------FIX--FINGERS-------------------'
-        data = {'curr': str(self.node_id), 'i': str(random.randint(0,self.m-1))}
+        print self.fingers
+        data = {'curr': str(self.node_id), 'i': str(random.randint(1,self.m-1))}
         channel.basic_publish(exchange='',
-                              routing_key='findsucc' + str(self.succ),
+                              routing_key='findsucc' + str(self.fingers[0]),
                               body=json.dumps(data))
 
     def loop_stabilise(self,channel):
         while True:
             #print "loop stabilise self /" + str(self.node_id)
-            #print "self succ /"+ str(self.succ)
+            #print "self succ /"+ str(self.fingers[0])
             channel.basic_publish(exchange='',
-                                  routing_key='findpred'+str(self.succ),
+                                  routing_key='findpred'+str(self.fingers[0]),
                                   body=str(self.node_id))
             time.sleep(15)
             self.fix_fingers(channel)
